20882e9c6d587fcc7921f9e73820cb89c9e5daea
Cherkashin Mikhail <cherkashinmikhailv@gmail.com>
2019-10-24 23:06:03 +0600

    Кеширование маршрутов и сущностей

---
+++ config/cache.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'reset_cache' => false,
+  'script_cache' => true,
+  'middleware_cache' => true,
+  'router_cache' => true,
+  'router_cache_method' => 'file',
+  'entities_cache' => true,
+  'entities_cache_method' => 'file'
+];
+?>
--- index.php
+++ index.php
@@ -20,6 +20,8 @@ $Data = new ReflectionClass('Test');
 var_dump($Data->getMethods()[0]->getDocComment());
 
 */
+$start = microtime(true);
+$server_start = memory_get_usage(); // Нагрузка на сервер
 
 require_once('vendor/autoload.php');
 
@@ -27,16 +29,29 @@ define('GLOBAL_DIR', __DIR__);
 //composer dump-autoload -o
 $Core = new Amper\Core();
 $Core->run();
-
+// var_dump(opcache_get_status());
 // $User = new App\Entities\UserEntity;
 // // $User->setId(3);
 // $User->setName('Bobster');
 // $User->setFname('Bob fname new');
 // $User->getMyName();
-$UserRepo = new App\Repositories\UserRepo;
-$UserRepo->remove(5);
+// $UserRepo = new App\Repositories\UserRepo;
+// $UserRepo->remove(5);
 // var_dump($UserRepo->save($User));
 // var_dump($UserRepo->findAllByIdOrName(1,'zxc'));
 // $UserRepo->findByIdOrderByIdLimit(1,5);
 // var_dump($UserRepo->findAll());
+
+function FBytes($bytes, $precision = 2) {
+    $units = array('B', 'KB', 'MB', 'GB', 'TB');
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes?log($bytes):0)/log(1024));
+    $pow = min($pow, count($units)-1);
+    $bytes /= pow(1024, $pow);
+    return round($bytes, $precision).' '.$units[$pow];
+}
+
+//print_r(E::$Errors);
+echo 'Памяти '.FBytes(memory_get_usage() - $server_start).'<br>'; // Нагрузка на сервер
+echo "Время выполнения скрипта: ".(microtime(true) - $start).'<br>';
 ?>
